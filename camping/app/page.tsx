900cbc10f0e50009d2ea3895dad68927af748cc5
yu88569 <chiangta.jirayu@gmail.com>
2024-11-20 00:18:17 +0700

    .

---
+++ camping/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+
+const page = () => {
+  return (
+    <>
+    <div>page</div>
+    <h1>sss</h1>
+    <Button variant="outline">Button</Button>
+    </>
+    
+  )
+}
+export default page
